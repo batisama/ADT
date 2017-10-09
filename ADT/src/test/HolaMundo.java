50cbfa35e86f64f8d3b835784dcb48068d444e0f
Matias <Matias@192.168.1.100>
2017-10-09 16:20:58 -0300

    hello loquillo

--- ADT/src/test/HolaMundo.java
+++ ADT/src/test/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
     
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        JOptionPane.showMessageDialog(null, "Hola Mundo, como va?");
     }
     
 }
